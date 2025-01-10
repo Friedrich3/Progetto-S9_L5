59216ec66c1c007e7930d4e3dad8469231bf7db9
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-10 16:43:39 +0100

    Aggiunto footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import MyNavbar from "./components/MyNavbar";
+import MyFooter from "./components/MyFooter";
 
 
 function App() {
   return <>
   <MyNavbar />
+  <MyFooter />
   </>;
 }
 
