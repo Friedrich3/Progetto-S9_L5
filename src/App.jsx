ced52f328a0fef68118b4f503d2a42c22b52d492
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-11 13:04:05 +0100

    refactor account page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import MyNavbar from "./components/MyNavbar";
 import MyFooter from "./components/MyFooter";
 import MyHome from "./components/MyHome";
+import AccountPage from "./components/AccountPage";
+
 
 
 function App() {
   return <>
   <MyNavbar />
-  <MyHome />
+  {/* <MyHome /> */}
+  <AccountPage />
   <MyFooter />
   </>;
 }
